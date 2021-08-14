0fcf1804fb90afa4ff6f7209c04a0ddc369b5bab
Ludvig Janiuk <ludvig.j.janiuk@oracle.com>
2021-08-14 14:42:57 +0200

    partial orders

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 console.log("Hello world!");
 console.log("Testing the trading application!");
 
-const te = require("./tradingEngine.js").instance();
+var te = require("./tradingEngine.js").instance();
 
 // TODO proper money handling
-const orders = [
+var orders = [
 	{acc: "1", action: "sell", item: "1001",  qty: 1, unitPrice: 45},
 	{acc: "2", action: "buy", item: "1001",  qty: 1, unitPrice: 45},
 ]
 
-const expectedTransactions = [
+var expectedTransactions = [
 	{from: "1", to: "2", item: "1001", qty: 1, charge: 45}
 ]
 
@@ -38,4 +38,40 @@ assert(actualTransaction.item == expectedTransactions[0].item, "item");
 assert(actualTransaction.qty == expectedTransactions[0].qty, "qty");
 assert(actualTransaction.charge == expectedTransactions[0].charge, "charge");
 
-console.log("SUCCESS");
+console.log("SUCCESS 1");
+
+
+orders = [
+	{acc: "1", action: "sell", item: "1001",  qty: 4, unitPrice: 45},
+	{acc: "2", action: "buy", item: "1001",  qty: 3, unitPrice: 45},
+]
+
+expectedTransactions = [
+	{from: "1", to: "2", item: "1001", qty: 3, charge: 45*3}
+]
+
+expectedOrders = [
+	{acc: "1", action: "sell", item: "1001",  qty: 1, unitPrice: 45},
+]
+
+te = require("./tradingEngine.js").instance();
+
+te.registerOrder(orders[0]);
+te.registerOrder(orders[1]);
+assert(te.pendingTransactions().length == 1, "transacstions");
+assert(te.getOrders().length == 1, "orders");
+
+assert(te.pendingTransactions()[0].from == expectedTransactions[0].from, "tr from");
+assert(te.pendingTransactions()[0].to == expectedTransactions[0].to, "tr to");
+assert(te.pendingTransactions()[0].item == expectedTransactions[0].item, "tr item");
+assert(te.pendingTransactions()[0].qty == expectedTransactions[0].qty, "tr qty");
+assert(te.pendingTransactions()[0].charge == expectedTransactions[0].charge, "tr charge");
+
+assert(te.getOrders()[0].acc == expectedOrders[0].acc, "ord acc");
+assert(te.getOrders()[0].action == expectedOrders[0].action, "ord action");
+assert(te.getOrders()[0].item == expectedOrders[0].item, "ord item");
+assert(te.getOrders()[0].qty == expectedOrders[0].qty, "ord qty");
+assert(te.getOrders()[0].unitPrice == expectedOrders[0].unitPrice, "ord unitPrice");
+
+
+console.log("SUCCESS2");
